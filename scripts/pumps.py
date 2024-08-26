55b77a57a437124810ff62fa2e53bf39c20c6620
atnery <145217041+atnery@users.noreply.github.com>
2024-08-26 12:24:23 -0400

    Moved pump power function into pumps function

--- scripts/pumps.py
+++ scripts/pumps.py
@@ -1,13 +1,5 @@
 from CoolProp import CoolProp as cp
 import math
-# Known fluid properties
-fluid = 'oxygen' # fluid name
-P = 101325 # Pressure [Pa]
-T = 400 # Temperature [Kelvin]
-# Use CoolProp to find density
-D = cp.PropsSI('D', 'P', P, 'T', T, fluid) # Density [kg/m3]
-print(D)
-
 
 def calcPowerTorque(density, mass_flowrate, inletpsi, outletpsi, rpm):
     volumetric_flowrate = (mass_flowrate/density)*7.481*60 #convert from lbm/s to gpm
@@ -21,10 +13,18 @@ def calcPowerTorque(density, mass_flowrate, inletpsi, outletpsi, rpm):
 
     return power_hp, power_kw, torque
 
+def pumps():
+    # Known fluid properties
+    fluid = 'oxygen' # fluid name
+    P = 101325 # Pressure [Pa]
+    T = 400 # Temperature [Kelvin]
+    # Use CoolProp to find density
+    D = cp.PropsSI('D', 'P', P, 'T', T, fluid) # Density [kg/m3]
+    print(D)
 
-#OxPump
-ox_hp, ox_kw, ox_torque = calcPowerTorque(67.78, 2.82, 40, 360, 10000)
-print(ox_hp, " ", ox_torque)
+    #OxPump
+    ox_hp, ox_kw, ox_torque = calcPowerTorque(67.78, 2.82, 40, 360, 10000)
+    print(ox_hp, " ", ox_torque)
 
-fu_hp, fu_kw, fu_torque = calcPowerTorque(49.26, 1.65, 40, 500, 10000)
-print(fu_hp, " ", fu_torque)
+    fu_hp, fu_kw, fu_torque = calcPowerTorque(49.26, 1.65, 40, 500, 10000)
+    print(fu_hp, " ", fu_torque)
