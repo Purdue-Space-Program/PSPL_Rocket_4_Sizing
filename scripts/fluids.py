b64d7c04298e58f1e3e4e3745ce4c1e0bb1d3ad0
DeltaV1ms <htfilm1@gmail.com>
2024-09-12 21:28:45 -0400

    Added section to resize COPV for pump-fed

--- scripts/fluids.py
+++ scripts/fluids.py
@@ -24,7 +24,6 @@ import math as m
 from CoolProp.CoolProp import PropsSI
 
 def fluids_sizing(
-    pumps,
     oxidizer,
     fuel,
     mixRatio,
@@ -85,10 +84,7 @@ def fluids_sizing(
         fuDensity = 786 # [kg/m^3] IPA density
 
     # Tank pressure
-    if pumps:
-        pass # NEED TO DETERMINE METHOD
-    else:
-        tankPressure = chamberPressure / CHAMBER_DP_RATIO # [Pa] Tank pressure
+    tankPressure = chamberPressure / CHAMBER_DP_RATIO # [Pa] Tank pressure
 
     # Tank volumes
     heliumCv = PropsSI('CVMASS', 'P', 1 * ATM2PA, 'T', T_INF, 'helium') # [J/kgK] Constant-volume specific heat of helium at STP (assumed constant)
@@ -175,6 +171,92 @@ def fluids_sizing(
         * (tankOD**2 - tankID**2)
     ) # [1] Margin to buckling
 
+    # Return outputs
+
+# Resize COPV for pump-fed vehicle
+
+def pumpfed_fluids_sizing(
+        tankTotalVolume,
+        requiredNpsh
+        ):
+    
+    # Constants
+
+    # Conversions
+    PSI2PA = 6894.76 # [Pa/psi] Conversion factor from psi to Pa
+    ATM2PA = 101325 # [Pa/atm] Conversion factor from atm to Pa
+    L2M3 = 0.001 # [m^3/l] Conversion factor from l to m^3
+
+    # Allowable alternate COPVs
+    BZB_COPV_VOLUME = 9 * L2M3 # [m^3] Volume of the BZB COPV
+    BZB_COPV_PRESSURE = 4950 * PSI2PA # [Pa] Maximum pressure of the BZB COPV
+    BZ1_COPV_VOLUME = 1 * L2M3 # [m^3] Volume of the BZ1 COPV [TEMPORARY NEED TO ADD]
+    BZ1_COPV_PRESSURE = 1 * PSI2PA # [Pa] Maximum pressure of the BZ1 COPV [TEMPORARY NEED TO ADD]
+
+    # Propellant
+    T_INF = 20 + 273.15 # [K] Assumed ambient temperature
+    ULLAGE_PERCENT =  10 # [1] Percent of tank volume dedicated to ullage
+    R_PROP = (100 - ULLAGE_PERCENT) / 100 # [1] Ratio of total tank volume to total propellant volume
+
+    # Plumbing
+    PUMP_DP_RATIO = 1 # [1] Pump inlet pressure / tank pressure, based on past rockets [NEED TO ADD]
+    HE_GAS_CONSTANT = 2077.1 # [J/kgK] Helium gas constant
+    COPV_TEMP_1 = T_INF + 15 # [K] Assumed initial COPV temperature
+    BURNOUT_PRESSURE_RATIO = 2 # [1] COPV burnout pressure / tank pressure to ensure choked flow
+    K_PRESSURIZATION = 1.0 # [1] Ratio of ideal tank volume to actual tank volume [1 IS TEMPORARY, NEED TO FIND ACTUAL VALUE]
+
+    # Tank pressure using pumps
+    pumpTankPressure = requiredNpsh / PUMP_DP_RATIO # [Pa] Tank pressure
+
+    # Volume checks
+    heliumCv = PropsSI('CVMASS', 'P', 1 * ATM2PA, 'T', T_INF, 'helium') # [J/kgK] Constant-volume specific heat of helium at STP (assumed constant)
+
+    # BZ1 COPV volume check
+    copvPressure1 = BZ1_COPV_PRESSURE # [Pa] COPV initial pressure
+    copvPressure2 = BURNOUT_PRESSURE_RATIO * pumpTankPressure # [Pa] COPV burnout pressure
+
+    copvEntropy1 = PropsSI('S', 'P', copvPressure1, 'T', COPV_TEMP_1, 'helium') # [J/kgK] COPV initial specific entropy
+    copvEntropy2 = copvEntropy1 # [J/kgK] COPV burnout specific entropy (assumed isentropic expansion)
+
+    copvDensity1 = PropsSI('D', 'P', copvPressure1, 'T', COPV_TEMP_1, 'helium') # [kg/m^3] COPV initial density 
+    copvDensity2 = PropsSI('D', 'P', copvPressure2, 'S', copvEntropy2, 'helium') # [kg/m^3] COPV burnout density
+
+    copvEnergy1 = PropsSI('U', 'P', copvPressure1, 'T', COPV_TEMP_1, 'helium') # [J/kg] COPV initial specific energy
+    copvEnergy2 = PropsSI('U', 'P', copvPressure2, 'S', copvEntropy2, 'helium') # [J/kg] COPV burnout specific energy
+
+    tankMaxVolume= K_PRESSURIZATION * (
+        ((copvDensity1 * BZ1_COPV_VOLUME * copvEnergy1) - (copvDensity2 * BZ1_COPV_VOLUME * copvEnergy2)) 
+        / (pumpTankPressure * (heliumCv / HE_GAS_CONSTANT + R_PROP))
+    ) # [m^3] Maximum propellant tank volume with BZ1 COPV
+
+    if tankMaxVolume >= tankTotalVolume:
+        BZ1copvUsable = True
+    else:
+        BZ1copvUsable = False
+
+
+    # BZB COPV volume check
+    copvPressure1 = BZB_COPV_PRESSURE # [Pa] COPV initial pressure
+    copvPressure2 = BURNOUT_PRESSURE_RATIO * pumpTankPressure # [Pa] COPV burnout pressure
+
+    copvEntropy1 = PropsSI('S', 'P', copvPressure1, 'T', COPV_TEMP_1, 'helium') # [J/kgK] COPV initial specific entropy
+    copvEntropy2 = copvEntropy1 # [J/kgK] COPV burnout specific entropy (assumed isentropic expansion)
+
+    copvDensity1 = PropsSI('D', 'P', copvPressure1, 'T', COPV_TEMP_1, 'helium') # [kg/m^3] COPV initial density 
+    copvDensity2 = PropsSI('D', 'P', copvPressure2, 'S', copvEntropy2, 'helium') # [kg/m^3] COPV burnout density
+
+    copvEnergy1 = PropsSI('U', 'P', copvPressure1, 'T', COPV_TEMP_1, 'helium') # [J/kg] COPV initial specific energy
+    copvEnergy2 = PropsSI('U', 'P', copvPressure2, 'S', copvEntropy2, 'helium') # [J/kg] COPV burnout specific energy
+
+    tankMaxVolume = K_PRESSURIZATION * (
+        ((copvDensity1 * BZB_COPV_VOLUME * copvEnergy1) - (copvDensity2 * BZB_COPV_VOLUME * copvEnergy2)) 
+        / (pumpTankPressure * (heliumCv / HE_GAS_CONSTANT + R_PROP))
+    ) # [m^3] Maximum propellant tank volume with BZB COPV
+    
+    if tankMaxVolume >= tankTotalVolume:
+        BZBcopvUsable = True
+    else:
+        BZBcopvUsable = False
 
 fluids_sizing(False, "Methane", "Oxygen", 2.4, 2 * 10**6, 40 * 10**6, 9.01289 * 10**(-3), 6.625 * 0.0254, 6.357 * 0.0254)
 
