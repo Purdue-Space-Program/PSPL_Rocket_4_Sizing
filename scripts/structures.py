bf32e7df58a1802f1e5ce31352ac850166c6fd8b
Will Mattison <73208185+wmattiso@users.noreply.github.com>
2024-09-21 03:57:59 -0400

    more structures

--- scripts/structures.py
+++ scripts/structures.py
@@ -18,6 +18,7 @@
 # -  Prop Length [m]
 # -  Oxidized Flow Rate [kg/s]
 # -  Fuel Flow Rate [kg/s]
+# -  Burn Time [s]
 
 # -  OD [in]
 
@@ -50,7 +51,6 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import constants as c
 
-
 def structures(
     thrust,
     lowerPlumbingLength,
@@ -63,22 +63,49 @@ def structures(
     oxFlowRate,
     fuelFlowRate,
     OD,
+    burnTime,
+    tankLength
 ):
-
+    
     ### Constants and Inputs
 
     rocketArea = (OD / 2) * np.pi # [in^2] area of the rocket body
+    rocketArea = rocketArea * c.IN22M2 # [m^2]
+    alumDens = 2710 #[kg/m^3]
+    alumYoungMod = 69 * 10^9 #[Pa]
 
-    rocketArea = rocketArea * c.
+    recoveryMass = 10 #####################
+    structuresMassGuess = recoveryMass + 40 #[kg]
+    totMass = fluidSystemMass + propMass + (oxFlowRate + fuelFlowRate) * burnTime + COPVMass * c.LB2KG + structuresMassGuess #[kg]
+
+    CMSRecoveryLength = 12 * c.IN2M ############
+    CMSOD = 6.625 * c.IN2M #[m]
+    recoveryLength = (8 * CMSOD^2 * CMSRecoveryLength) / (OD^2) # [m]
+    noseConeLength = 30 * c.IN2M ################
+    totLength = propLength + lowerPlumbingLength + tankLength + upperPlumbingLength + COPVLength * c.IN2M + recoveryLength + noseConeLength #[m]
+
+    CdNoseCone = 0.45
 
 
 
-### Off-the Rail case
+    loop = True
+    while loop:
+        ### Off-the Rail Constants
 
-    AoARail = 10 * np.pi / 180 # [rad] Worst angle of attack off the rail
-    railThrust = thrust #[N]
+        AoARail = 10 * np.pi / 180 # [rad] Worst angle of attack off the rail
+        railThrust = thrust #[N]
+        airDensityRail = 1.112 #[kg/m^3]
+
+        inLineAccel = (-totMass + (totMass^2 + 2 * CdNoseCone * airDensityRail * rocketArea * railThrust)^0.5) / (CdNoseCone*airDensityRail*rocketArea) #[m/s^2]
+
+        
+        
+
+        ### Max Q Constants
+
     
 
-### Max Q Case
+        ### Axial Loads
 
-### Recovery Case
+        
+
