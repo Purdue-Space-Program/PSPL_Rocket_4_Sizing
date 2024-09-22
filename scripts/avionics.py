b00c4b95f36ece1a09f788b853cef0cea01e57fb
DeltaV1ms <htfilm1@gmail.com>
2024-09-22 16:50:15 -0400

    Fix avionics comments

--- scripts/avionics.py
+++ scripts/avionics.py
@@ -13,7 +13,7 @@ import constants as c
 
 def calculate_avionics():
 
-    mass = 6  # [lb] mass of the rocket
-    mass = 6 * c.LB2KG  # [kg] mass of the rocket
+    mass = 6  # [lb] estimated mass of avionics
+    mass = 6 * c.LB2KG  # [kg] estimated mass of avionics
 
     return [mass]
