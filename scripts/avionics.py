cec17e37cc9936e6b0fed701c16714060438c0f9
Nickolas Nielsen <nielsenn@purdue.edu>
2024-09-30 13:13:53 -0400

    standing in a field full of land mines doing moonwalks

--- scripts/avionics.py
+++ scripts/avionics.py
@@ -11,23 +11,68 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import constants as c
 
 
-def pumpfed_avionics_sizing(powerRequired, cellDF):
+def avionics_sizng():
+    mass = 6  # [lbm] mass of avionics
+    mass = mass * c.LBM2KG  # [kg] Convert mass to kg
+
+    return mass
+
+
+def pumpfed_avionics_sizing(powerRequired, motorDF, cellDF):
 
     # Constants
+    BASE_AVI_MASS = 6  # [lbm] base mass of avionics
+    BASE_AVI_MASS = BASE_AVI_MASS * c.LBM2KG  # [kg] Convert mass to kg
+
     MOTOR_EFFICIENCY = 0.85  # estimated motor efficiency
+
     LIPO_CELL_VOLTAGE = 22.2  # [V] nominal voltage of a LiPo cell
     LIPO_CELL_DISCHARGE_CURRENT = 80  # [A] maximum discharge current of a LiPo cell
-    LIPO_CELL_MASS = 22.2  # [g] mass of a LiPo cell
 
+    LIPO_CELL_MASS = 22.2  # [g] mass of a LiPo cell
     LIPO_CELL_MASS = LIPO_CELL_MASS * c.G2KG  # [kg] Convert mass to kg
 
-    power = powerRequired / MOTOR_EFFICIENCY  # [W] power required for avionics
+    # Power required (adjusted for motor efficiency)
+    totalPowerRequired = powerRequired / MOTOR_EFFICIENCY  # [W]
 
-    
-        numberOfTotalCells = numberOfSeriesCells * numberOfParallelCells
+    optimal_solution = None
+    min_total_mass = float("inf")
 
-    return [
-        totalAviMass,
-        numberOfParallelCells,
-        numberOfSeriesCells,
-    ]
+    for index, motor in motorDF.iterrows():
+        motor_voltage = motor["Max Volts"]
+        motor_current = totalPowerRequired / motor_voltage
+
+        # Check if motor can handle the current
+        if motor_current > motor["Max Amps"]:
+            continue
+
+        # Number of series cells to meet the voltage requirement
+        numSeriesCells = motor_voltage / LIPO_CELL_VOLTAGE
+        numSeriesCells = np.ceil(numSeriesCells)  # Round up
+
+        # Number of parallel cells to meet the current requirement
+        numParallelCells = motor_current / LIPO_CELL_DISCHARGE_CURRENT
+        numParallelCells = np.ceil(numParallelCells)  # Round up
+
+        # Total number of cells and mass of the battery pack
+        totalCells = numSeriesCells * numParallelCells
+        totalBatteryMass = totalCells * LIPO_CELL_MASS
+
+        # Total mass (motor + battery)
+        total_mass = totalBatteryMass + motor["Motor Mass [kg]"]
+
+        # Track the optimal solution (minimize total mass)
+        if total_mass < min_total_mass:
+            min_total_mass = total_mass
+            optimal_solution = [
+                total_mass,
+                numSeriesCells,
+                numParallelCells,
+                totalCells,
+                totalBatteryMass,
+                motor["Motor Mass [kg]"],
+                motor["Max Amps"],
+                motor["Max Volts"],
+            ]
+
+    return optimal_solution
