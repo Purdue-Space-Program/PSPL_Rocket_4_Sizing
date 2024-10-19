73fb1417e36f117be54dee88574f73cb4f37d270
atnery <andrewtnery@gmail.com>
2024-10-19 11:21:50 -0400

    ummmm uhhhhhh

--- constants.py
+++ constants.py
@@ -149,6 +149,7 @@ BZ1_COPV_MASS = 3  # [kg] Mass of BZ1 COPV
 ## All motor constans are based on the Neumotors 2020 Series motor
 MOTOR_WEIGHT = 0.660  # [kg] weight of a single motor
 MOTOR_LENGTH = 0.093  # [m] length of a single motor
+MOTOR_DIAMETER = 3.1 * IN2M
 
 # LIPO Battery Constants
 LIPO_CELL_MASS = 0.400  # [kg] mass of a LiPo cell
